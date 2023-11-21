5bfd7e7f8631c1eb55532d9ed9214bed95b1cbd9
kkt <670335957@qq.com>
2023-11-21 20:56:25 +0800

    add: 添加页面跳转功能

--- src/View/ShoppingRecommend/src/components/SearchBar/index.tsx
+++ src/View/ShoppingRecommend/src/components/SearchBar/index.tsx
@@ -1,16 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import './index.scss'
+import axios from 'axios';
 import { Input} from 'antd';
 const { Search } = Input;
 
 export default function SearchBar() {
 
-    const onSearch = () => {
-        console.log('onSearch');
-      }
+    const navigate = useNavigate();
+
+    const onSearch = (value: any) => {
+      navigate('/Result');
+      axios.get(`https://funky-amoeba-steady.ngrok-free.app/Search?name=${value}`).then(
+        res => {
+          console.log('res->', res);
+        },
+        err => {
+          console.log('err->', err);
+          
+        }
+      )
+    } 
 
   return (
     <div className="SearchBar">
-        <Search placeholder="输入详细商品信息" classNames={{input: 'search'}} onSearch={onSearch} enterButton />
+        <Search placeholder="输入详细商品信息" classNames={{input: 'search'}} onSearch={onSearch} enterButton/>
     </div>
   )
 }
